a0cbc8ea6fa473f2422321e9cc91eaed46fb4e27
DoctorTone <drtonesoftware@gmail.com>
2020-04-29 19:27:27 +0100

    Added new video and autoplay.

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+$(document).ready( () => {
+    // DEBUG
+    console.log("About to play...");
+
+    /*
+    const vidElement = document.getElementById("showReel");
+    vidElement.autoplay = true;
+    vidElement.load();
+    */
+});
