864eef55aea67a07b465603314bcb699d57717d2
DRT Software <tony@drt-software.com>
2020-05-13 15:59:07 +0100

    Updates to main page.

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,10 @@ import bootstrap from "bootstrap";
 
 const CTA_TIMEOUT = 10 * 1000;
 
-setTimeout( () => {
-    $('#CTAModal').modal();
-}, CTA_TIMEOUT);
-
 $(document).ready( () => {
-
-    
+    /*
+    setTimeout( () => {
+        $('#CTAModal').modal();
+    }, CTA_TIMEOUT);
+    */
 });
