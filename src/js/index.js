c4fdd27e0f6b9fe19b27520a4abbc56cef1bf28a
DoctorTone <drtonesoftware@gmail.com>
2020-05-27 16:07:47 +0100

    Added glide.js slider.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 import $ from "jquery";
 import bootstrap from "bootstrap";
+import Glide from "@glidejs/glide";
 
 const CTA_TIMEOUT = 10 * 1000;
 
 $(document).ready( () => {
-    /*
-    setTimeout( () => {
-        $('#CTAModal').modal();
-    }, CTA_TIMEOUT);
-    */
+    const glide = new Glide("#newsCarousel", {
+        type: "carousel",
+        perView: 3
+    });
+
+    glide.mount();
 });
