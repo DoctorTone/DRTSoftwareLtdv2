8e7e53efacce97dd682d3bcae0dabce238ca2bc3
DoctorTone <drtonesoftware@gmail.com>
2020-05-28 15:10:48 +0100

    Play video accordig to resolution.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import $ from "jquery";
-import bootstrap from "bootstrap";
 import Glide from "@glidejs/glide";
 
 const CTA_TIMEOUT = 10 * 1000;
+const MAX_MOBILE_VIDEO_WIDTH = 768;
+let playingMobile = false;
 
 $(document).ready( () => {
     const glide = new Glide("#newsCarousel", {
@@ -17,4 +18,31 @@ $(document).ready( () => {
     });
 
     glide.mount();
+
+    let forcePlay = true;
+    playShowReel(forcePlay);
+
+    // Resize events
+    window.addEventListener("resize", playShowReel);
 });
+
+function playShowReel(forcePlay) {
+    const videoElem = document.getElementById("showReel");
+    const videoMobile = "./showReelMobile.mp4";
+    const videoDesktop = "./showReel2.mp4";
+    if (window.innerWidth < MAX_MOBILE_VIDEO_WIDTH) {
+        if (!playingMobile) {
+            videoElem.src = videoMobile;
+            videoElem.load();
+            videoElem.play();
+            playingMobile = true;
+        }
+    } else {
+        if (playingMobile || forcePlay) {
+            videoElem.src = videoDesktop;
+            videoElem.load();
+            videoElem.play();
+            playingMobile = false;
+        }
+    }
+}
