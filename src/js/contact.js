b7888b6a857a6451fd294598c828e49f9e649ada
DoctorTone <drtonesoftware@gmail.com>
2020-06-03 00:38:09 +0100

    Validate contact form.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,5 +1,16 @@
 import $ from "jquery";
 
 $(document).ready( () => {
+    // Elements
+    const contactForm = $("#contactForm");
+    const service = $("#service");
 
+    contactForm.submit( () => {
+        if (service.val() === "0") {
+            alert("Please select a service");
+            return false;
+        }
+
+        return true;
+    })
 });
