91c0cc8144852442d39a25938ea877916d559aa4
DoctorTone <drtonesoftware@gmail.com>
2020-07-29 00:35:45 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 import Glide from "@glidejs/glide";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'; // Import precompiled Bootstrap css
-import '@fortawesome/fontawesome-free/css/all.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+import "@fortawesome/fontawesome-free/css/all.css";
 import "../../node_modules/@glidejs/glide/dist/css/glide.core.min.css";
 import "../css/drtStyles.css";
 
@@ -16,69 +16,68 @@ const debugText = $("#DebugText");
 // Resize events
 window.addEventListener("resize", playShowReel);
 
-$(document).ready( () => {
-    playShowReel();
-    
-    const glide = new Glide("#newsCarousel", {
-        type: "carousel",
-        perView: 3,
-        breakpoints: {
-            640: {
-                perView: 1
-            },
-            992: {
-                perView: 2
-            }
-        },
-        autoplay: NEWS_SLIDER_TIME
-    });
+$(document).ready(() => {
+	playShowReel();
 
-    glide.mount();
-    
+	const glide = new Glide("#newsCarousel", {
+		type: "carousel",
+		perView: 3,
+		breakpoints: {
+			640: {
+				perView: 1,
+			},
+			992: {
+				perView: 2,
+			},
+		},
+		autoplay: NEWS_SLIDER_TIME,
+	});
+
+	glide.mount();
 });
 
 function playShowReel() {
-    const videoElem = document.getElementById("showReel");
-    const vidContainer = document.getElementById("vidContainer");
-    const videoMobile = "./showReelMobile3-1.webm";
-    const videoDesktop = "./showReel3-1.mp4";
-    if (process.env.NODE_ENV !== "production") {
-        videoElem.onerror = function() {
-            console.log("Error in video");
-        }
-        videoElem.onplay = function() {
-            console.log("Video is playing");
-        }
-        videoElem.onabort = function() {
-            console.log("Video aborted");
-        }
-        videoElem.onwaiting = function() {
-            console.log("Video waiting...");
-        }
-        videoElem.onpause = function() {
-            console.log("Video paused");
-        }
-        videoElem.onstalled = function() {
-            console.log("Video stalled");
-        }
-        videoElem.onsuspend = function() {
-            console.log("Video suspended");
-        }
-    }
-    
-    if (window.innerWidth < MAX_MOBILE_VIDEO_WIDTH) {
-        if (!playingMobile) {
-            videoElem.src = videoMobile;
-            videoElem.load();
-            playingMobile = true;
-            playing = true;
-        }
-    } else {
-        if (playingMobile || !playing) {
-            videoElem.src = videoDesktop;
-            videoElem.load();
-            playingMobile = false;
-            playing = true;
-        }
-    }
+	const videoElem = document.getElementById("showReel");
+	const vidContainer = document.getElementById("vidContainer");
+	const videoMobile = "./showReelMobilev5.webm";
+	const videoDesktop = "./showReel3-1.mp4";
+	if (process.env.NODE_ENV !== "production") {
+		videoElem.onerror = function () {
+			console.log("Error in video");
+		};
+		videoElem.onplay = function () {
+			console.log("Video is playing");
+		};
+		videoElem.onabort = function () {
+			console.log("Video aborted");
+		};
+		videoElem.onwaiting = function () {
+			console.log("Video waiting...");
+		};
+		videoElem.onpause = function () {
+			console.log("Video paused");
+		};
+		videoElem.onstalled = function () {
+			console.log("Video stalled");
+		};
+		videoElem.onsuspend = function () {
+			console.log("Video suspended");
+		};
+	}
+
+	if (window.innerWidth < MAX_MOBILE_VIDEO_WIDTH) {
+		if (!playingMobile) {
+			videoElem.src = videoMobile;
+			videoElem.load();
+			playingMobile = true;
+			playing = true;
+		}
+	} else {
+		if (playingMobile || !playing) {
+			videoElem.src = videoDesktop;
+			videoElem.load();
+			playingMobile = false;
+			playing = true;
+		}
+	}
 }
